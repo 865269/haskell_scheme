38d9e1357f998f657765c28735940d9b503ab82b
Matthew Diggory <mattydiggory@gmail.com>
2024-06-13 21:49:51 +0100

    added symbol parser using parsec package

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,17 @@
 module Main where
 
+import Text.ParserCombinators.Parsec hiding (spaces)
+import System.Environment
+
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = do 
+        (expr:_) <- getArgs
+        putStrLn (readExpr expr)
+
+symbol :: Parser Char
+symbol = oneOf "!#$%&|*+-/:<=>?@^_~"
+
+readExpr :: String -> String
+readExpr input = case parse symbol "lisp" input of
+    Left err -> "No match: " ++ show err
+    Right val -> "Found value"
